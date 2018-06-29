db9c0c58f13baf9fb877b1bdaa4f8ea05a205bdc
ChiefTwoPencils <robertwilk916@gmail.com>
2018-06-28 17:45:28 -0700

    Added docs to RecordController.

--- OneOhOne/ApiControllers/RecordController.cs
+++ OneOhOne/ApiControllers/RecordController.cs
@@ -4,12 +4,27 @@ using System.Web.Http;
 
 namespace OneOhOne.ApiControllers
 {
+    /// <summary>
+    /// Example class that accepts a service interface for testability.
+    /// No DI is involved; only an extra contructor is provided to allow
+    /// tests to inject faked implementations.
+    /// </summary>
     public class RecordController : ApiController
     {
+        // It is crucial to program to an interface rather
+        // than a concrete implementation used in production.
         private readonly IRecordService _service;
 
+        /// <summary>
+        /// Default constructor that falls back to the production service
+        /// when not under test.
+        /// </summary>
         public RecordController() : this(new RecordService()) { }
 
+        /// <summary>
+        /// Controller used to facilitate faked tests.
+        /// </summary>
+        /// <param name="recordService">The injected service.</param>
         public RecordController(IRecordService recordService)
         {
             _service = recordService;
